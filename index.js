54e69d3554d50e0a0bafee807e7cec953e950c37
Riya Bansal <35702912+riyabansal98@users.noreply.github.com>
2022-01-05 22:31:09 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
-// function hello(){
+const express = require('express'); 
+const { Sequelize } = require('sequelize');
+const dbConfig = require('./config.json')
+const app = express(); // this function call creates a new express application
+const port = 3000; //port where application will run
+const env = "development";
+const dbSettings = dbConfig[env];
+const sequelize = new Sequelize(
+    dbSettings.database, 
+    dbSettings.username,
+    dbSettings.password,
+    dbSettings
+);
 
-//     console.log("Hello World");
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, async () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+});
+
+
+// {
+//     "development": {
+//         "username": "root",
+//         "password": "root",
+//         "database": "relevel",
+//         "host": "127.0.0.1",
+//         "dialect": "mysql"
+//     },
+//     "production": {
+//         "user": "adminUser"
+//     }
 // }
 
-// hello();
+// Connecting Database to Application
+// Start MySQL server -> mysql -u root -p
+// CREATE Database Relevel;
 
-// var http = require('http');
-// var server = http.createServer()
-
-const fs = require('fs')
-
-// fs.rename('oldFile.txt', 'newFile.txt', (err) => {
-//     if (err) throw err;
-//     console.log('Rename complete!');
-// });
-
-// fs.readFile('file.txt',"utf8" ,(err, data) => {
-//     if (err) throw err;
-//     console.log(data);
-// });
-
-//UTF -> is an encoder
-const data = new Uint8Array(Buffer.from('Hello Node.js'));
-fs.writeFile('message.txt', data, (err) => {
-if (err) throw err;
-console.log('The file has been saved!');
-});
+// npm install mysql2
+// npm install sequelize
